26cbb3eec53065d848b1a3725bed2b57c6fbcd83
Jeija <norrepli@gmail.com>
2020-08-17 22:49:49 +0200

    Add configuration for HT32SX

--- src/stm32renard_gpio.h
+++ src/stm32renard_gpio.h
@@ -4,13 +4,13 @@
 #define _STM32RENARD_GPIO_H
 
 /* The pin configured here must correspond to S2-LP's GPIO3! */
-#define STM32RENARD_GPIO_PORT                        GPIOC
-#define STM32RENARD_GPIO_PIN                         GPIO_PIN_0
-#define STM32RENARD_GPIO_CLOCK_ENABLE()              __GPIOC_CLK_ENABLE()
+#define STM32RENARD_GPIO_PORT                        GPIOB
+#define STM32RENARD_GPIO_PIN                         GPIO_PIN_2
+#define STM32RENARD_GPIO_CLOCK_ENABLE()              __GPIOB_CLK_ENABLE()
 #define STM32RENARD_GPIO_SPEED                       GPIO_SPEED_HIGH
 #define STM32RENARD_GPIO_PUPD                        GPIO_NOPULL
 #define STM32RENARD_GPIO_EXTI_MODE                   GPIO_MODE_IT_FALLING
-#define STM32RENARD_GPIO_EXTI_IRQN                   EXTI0_1_IRQn
+#define STM32RENARD_GPIO_EXTI_IRQN                   EXTI2_3_IRQn
 
 void stm32renard_gpio_init(void);
 void stm32renard_gpio_interrupt_enable(bool enable);
